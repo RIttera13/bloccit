f0dde82b4d1c055c4336e056e3a9a1c850845a36
Drew <rittera13@gmail.com>
2016-10-22 17:49:11 -0500

    Ruby checkpoint 20, create edit and delete methods, test, and button in the view. Tested all functions and tests pass.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,6 @@
 class PostsController < ApplicationController
   def index
-    bad_word = 710
     @posts = Post.all
-    @posts.each do |item|
-      if bad_word % 5 == 0
-        item.title = "SPAM"
-        item.save
-        bad_word += 1
-      else
-        bad_word += 1
-      end
-    end
   end
 
   def show
@@ -36,6 +26,32 @@ class PostsController < ApplicationController
   end
 
   def edit
+     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
 end
