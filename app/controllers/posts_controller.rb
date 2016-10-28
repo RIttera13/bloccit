8e67ad9b80566addadc494c205276b9d593c14a3
Drew <rittera13@gmail.com>
2016-10-28 16:20:29 -0500

    Ruby assignment 28, created a new user role 'moderator'. The role is a blend between a member and admin, with litmited admin capabilitys. The moderator is able to create posts, edit posts/topics, delete own posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,9 +61,11 @@ class PostsController < ApplicationController
 
   def authorize_user
     post = Post.find(params[:id])
-    unless current_user == post.user || current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
+    unless current_user == post.user || current_user.moderator? || current_user.admin?
+      flash[:alert] = "You must be a moderator/admin to do that."
       redirect_to [post.topic, post]
     end
   end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,10 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:admin?)
     end
 
+    it "responds to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
+
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
@@ -43,6 +47,27 @@ RSpec.describe User, type: :model do
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
+      end
+
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_falsey
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "moderator user" do
+      before do
+        user.moderator!
+      end
+      it "returns true for #moderator?" do
+        expect(user.moderator?).to be_truthy
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
       end
 
       it "returns false for #admin?" do
