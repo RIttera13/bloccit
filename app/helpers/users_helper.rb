76e4c7171b38bfbcdfa184f5cc8460a93f2161d0
Drew <rittera13@gmail.com>
2016-11-07 22:19:40 -0600

    Ruby assignment 32, added new factories for comments and vote spec. Added new features to user profile. Added a User Post, User Comments, and User Favorites list. Attached the current rank/OP Gravitar/Comment Count for the user favorites.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def has_post?(user)
+      @user.posts.count > 0
+    end
+
+    def has_comment?(user)
+      @user.comments.count > 0
+    end
+
+    def has_favorites?(user)
+      @user.favorites.count > 0
+    end
 end
